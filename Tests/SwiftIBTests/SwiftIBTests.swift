d0b3636ba9703f3334a614d543cc308766a69529
Harry Li <harryl.byread@gmail.com>
2019-10-23 11:23:43 +1100

    Use Xcode for SPM

--- Tests/SwiftIBTests/SwiftIBTests.swift
+++ Tests/SwiftIBTests/SwiftIBTests.swift
@@ -2,14 +2,14 @@ import XCTest
 @testable import SwiftIB
 
 final class SwiftIBTests: XCTestCase {
-    func testExample() {
+    func testTickType() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SwiftIB().text, "Hello, World!")
+        XCTAssertEqual(SwiftIB.TickType.getField(99), "unknown")
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testTickType", testTickType),
     ]
 }
